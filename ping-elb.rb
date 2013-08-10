8ac5907d8197f50aada9432e810f72285cca16de
Charles Hooper <chooper@plumata.com>
2013-08-10 15:04:51 -0700

    Use environment vars when available and some clean-up

--- ping-elb.rb
+++ ping-elb.rb
@@ -8,23 +8,28 @@ require "net/http"
 
 include Net::DNS
 
-MAX_VERB_LENGTH = 127
+MAX_VERB_LENGTH = ENV['PING_ELB_MAXVERBLEN'] || 127
 DEFAULT_NAMESERVER = ENV['PING_ELB_NS'] || 'ns-941.amazon.com'
-NS_ADDRS = Resolver(DEFAULT_NAMESERVER).answer.map { |rr| rr.address.to_s }
-DEBUG = false
-DEFAULT_PING_COUNT = 4
+DEFAULT_PING_COUNT = ENV['PING_ELB_PINGCOUNT'] || 4 # TODO: override by opts
+DEBUG = ENV['PING_ELB_DEBUG']
 
+# Resolve the nameserver hostname to a list of IP addresses
+NS_ADDRS = Resolver(DEFAULT_NAMESERVER).answer.map { |rr| rr.address.to_s }
+
+# Catch ctrl-c
 trap("INT") {
     puts "Received interrupt, exiting..."
     exit
 }
 
+# Define custom request method
 class ElbPing < Net::HTTPRequest
   METHOD = "A" * (MAX_VERB_LENGTH + 1)
   REQUEST_HAS_BODY = false
   RESPONSE_HAS_BODY = false
 end
 
+# Make HTTP request to given node using custom request method
 def ping_node(node, port=80, path="/")
   start = Time.now.getutc
   http = Net::HTTP.new(node, port.to_s)
@@ -35,6 +40,7 @@ def ping_node(node, port=80, path="/")
     :duration => (Time.now.getutc - start)}
 end
 
+# Resolve an ELB address to a list of node IPs
 def find_elb_nodes(target)
   resolver = Net::DNS::Resolver.new(
     :use_tcp => true,
@@ -52,6 +58,7 @@ def find_elb_nodes(target)
   nodes
 end
 
+# Format and display the ping data
 def display_response(status)
     node = status[:node]
     code = status[:code]
@@ -60,18 +67,25 @@ def display_response(status)
     puts "Response from #{node}: code=#{code} time=#{(duration * 1000).to_i} ms"
 end
 
-if ARGV.size < 1
-  puts "Usage: #{$0} <elb_hostname>"
-  exit(false)
+# Main entry point of the program
+def main
+    if ARGV.size < 1
+      puts "Usage: #{$0} <elb_hostname>"
+      exit(false)
+    end
+
+    target = ARGV[0]
+    nodes = find_elb_nodes(target)
+
+    # TODO: Display summary of results (in aggregate and per-node)
+    (1..DEFAULT_PING_COUNT).each { |i|
+        nodes.map { |node|
+            status = ping_node(node)
+            display_response(status)
+        }
+    }
 end
 
-target = ARGV[0]
-nodes = find_elb_nodes(target)
+main
 
-# TODO: Display summary of results (in aggregate and per-node)
-(1..DEFAULT_PING_COUNT).each { |i|
-    nodes.map { |node|
-        status = ping_node(node)
-        display_response(status)
-    }
-}
+
