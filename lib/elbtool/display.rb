56f72ff74499ec0013d013a22ced1ba808df46a8
Charles Hooper <chooper@plumata.com>
2013-08-11 12:22:46 -0700

    Move display functions to their own submodule

---
+++ lib/elbtool/display.rb
@@ -0,0 +1,42 @@
+
+module ElbTool
+  module Display
+    # Format and display the ping data
+    def self.response(status)
+      node = status[:node]
+      code = status[:code]
+      duration = status[:duration]
+
+      puts "Response from #{node}: code=#{code.to_s} time=#{duration} ms"
+    end
+
+    # Display summary of results (in aggregate and per-node)
+    def self.summary(total_summary, node_summary)
+      requests = total_summary[:reqs_attempted]
+      responses = total_summary[:reqs_completed]
+      loss = (1 - (responses.to_f/requests)) * 100
+
+      latencies = total_summary[:latencies]
+      avg_latency = (latencies.inject { |sum, el| sum + el }.to_f / latencies.size).to_i # ms
+
+      puts '--- total statistics ---'
+      puts "#{requests} requests, #{responses} responses, #{loss.to_i}% loss"
+      puts "min/avg/max = #{latencies.min}/#{avg_latency}/#{latencies.max} ms"
+
+      node_summary.each { |node, summary|
+        requests = summary[:reqs_attempted]
+        responses = summary[:reqs_completed]
+        loss = (1 - (responses.to_f/requests)) * 100
+
+        latencies = summary[:latencies]
+        avg_latency = (latencies.inject { |sum, el| sum + el }.to_f / latencies.size).to_i # ms
+
+        puts "--- #{node} statistics ---"
+        puts "#{requests} requests, #{responses} responses, #{loss.to_i}% loss"
+        puts "min/avg/max = #{latencies.min}/#{avg_latency}/#{latencies.max} ms"
+      }
+    end
+
+  end
+end
+
--- ping-elb.rb
+++ ping-elb.rb
@@ -7,6 +7,7 @@ require 'optparse'
 
 require './lib/elbtool/pinger.rb'
 require './lib/elbtool/resolver.rb'
+require './lib/elbtool/display.rb'
 
 $stderr.sync = true
 $stdout.sync = true
@@ -47,42 +48,6 @@ def usage
 end
 PARSER.parse!(ARGV) rescue usage
 
-# Format and display the ping data
-def display_response(status)
-  node = status[:node]
-  code = status[:code]
-  duration = status[:duration]
-
-  puts "Response from #{node}: code=#{code.to_s} time=#{duration} ms"
-end
-
-# Display summary of results (in aggregate and per-node)
-def display_summary(total_summary, node_summary)
-  requests = total_summary[:reqs_attempted]
-  responses = total_summary[:reqs_completed]
-  loss = (1 - (responses.to_f/requests)) * 100
-
-  latencies = total_summary[:latencies]
-  avg_latency = (latencies.inject { |sum, el| sum + el }.to_f / latencies.size).to_i # ms
-
-  puts '--- total statistics ---'
-  puts "#{requests} requests, #{responses} responses, #{loss.to_i}% loss"
-  puts "min/avg/max = #{latencies.min}/#{avg_latency}/#{latencies.max} ms"
-
-  node_summary.each { |node, summary|
-    requests = summary[:reqs_attempted]
-    responses = summary[:reqs_completed]
-    loss = (1 - (responses.to_f/requests)) * 100
-
-    latencies = summary[:latencies]
-    avg_latency = (latencies.inject { |sum, el| sum + el }.to_f / latencies.size).to_i # ms
-
-    puts "--- #{node} statistics ---"
-    puts "#{requests} requests, #{responses} responses, #{loss.to_i}% loss"
-    puts "min/avg/max = #{latencies.min}/#{avg_latency}/#{latencies.max} ms"
-  }
-end
-
 # Main entry point of the program
 def main
   if ARGV.size < 1
@@ -103,7 +68,7 @@ def main
 
   # Catch ctrl-c
   trap("INT") {
-    display_summary(total_summary, node_summary)
+    ElbTool::Display.summary(total_summary, node_summary)
     exit
   }
 
@@ -122,10 +87,10 @@ def main
         node_summary[node][:latencies] += [status[:duration]]
       end
 
-      display_response(status)
+      ElbTool::Display.response(status)
     }
   }
-  display_summary(total_summary, node_summary)
+  ElbTool::Display.summary(total_summary, node_summary)
 end
 
 main
