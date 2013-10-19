ee022fefce0cc79cafe154f1d1ea3e72a8180c02
Charles Hooper <chooper@plumata.com>
2013-10-19 12:45:32 -0700

    remove hardcoded elb from tests

--- test/test_resolver.rb
+++ test/test_resolver.rb
@@ -2,8 +2,6 @@
 require 'test/unit'
 require 'elbping/resolver.rb'
 
-DEFAULT_GOOD_ELB = ENV['TEST_GOOD_ELB'] || 'test-elb-868888812.us-east-1.elb.amazonaws.com' # feels dirty
-
 class TestResolver< Test::Unit::TestCase
   def test_bad_queries
     ["fake.amazonaws.com", "google.com", "nxdomain.asdf"].each { |tgt|
@@ -15,6 +13,8 @@ class TestResolver< Test::Unit::TestCase
 
   # This might still fail from time to time :-\ Been seeing lots of failures to connect to AWS DNS
   def test_good_query
+    return nil unless ENV['TEST_GOOD_ELB']
+
     resp = nil
     assert_nothing_raised do
       resp = ElbPing::Resolver.find_elb_nodes(DEFAULT_GOOD_ELB)
