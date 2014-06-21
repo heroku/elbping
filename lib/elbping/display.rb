5195e45c0cf24b7fb0b198770707981156d9778c
Charles Hooper <chooper@plumata.com>
2014-06-21 06:58:58 -0700

    another formatting change

--- lib/elbping/display.rb
+++ lib/elbping/display.rb
@@ -53,7 +53,7 @@ module ElbPing
       exc_display = exc ? "exception=#{exc}" : ''
       ssl_display = (sslSubject and sslExpires) ? "ssl cn=#{sslSubject} expires=#{sslExpires}" : ''
 
-      self.out "Response from: #{node.rjust(15)}: code=#{code.to_s} time=#{duration} ms #{ssl_display} #{exc_display}"
+      self.out "Response from: #{node.rjust(15)}: code=#{code.to_s} time=#{duration}ms #{ssl_display} #{exc_display}"
     end
 
     # Display summary of requests, responses, and latencies (for aggregate and per-node)
