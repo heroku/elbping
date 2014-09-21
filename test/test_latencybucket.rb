96f3623dfaaf2585e6dccfa003bad6802bdb5b77
Charles Hooper <chooper@plumata.com>
2014-09-21 13:20:38 -0700

    clean up latencybucket tests

--- test/test_latencybucket.rb
+++ test/test_latencybucket.rb
@@ -3,40 +3,32 @@ require 'elbping/latency_bucket.rb'
 
 class TestLatencyBucket < Test::Unit::TestCase
   def test_init
-    bucket = nil
-    assert_nothing_raised do
-      bucket = LatencyBucket.new
-      assert_not_nil bucket
-      bucket << 1
-      bucket << 2
-    end
+    bucket = LatencyBucket.new
+    assert_not_nil bucket
+    bucket << 1
+    bucket << 2
     assert_equal bucket.to_a, [1, 2]
   end
   def test_sum
     bucket = LatencyBucket.new
+
+    # bucket.sum should initialize to zero
+    assert_equal bucket.sum, 0
+
+    # and still
     bucket << 0
-    assert_nothing_raised do
-      assert_equal bucket.sum, 0
-    end
-    bucket << 100
-    bucket << 200
-    bucket << 300
-    bucket << 3
-    assert_equal bucket.sum, (0 + 100 + 200 + 300 + 3)
+    assert_equal bucket.sum, 0
+    bucket << 1
+    assert_equal bucket.sum, 1
   end
   def test_mean
     bucket = LatencyBucket.new
-    bucket << 100
-    bucket << 200
-    bucket << 300
-    bucket << 0
-    bucket << 3
-    assert_equal bucket.mean, ((100 + 200 + 300 + 0 + 3)/5).to_i
-  end
-  def test_mean_divide_by_zero
-    bucket = LatencyBucket.new
-    assert_nothing_raised do
-      assert_equal bucket.mean, 0
-    end
+    assert_equal bucket.mean, 0
+    bucket << 2
+    assert_equal bucket.mean, 2
+    bucket << 2
+    assert_equal bucket.mean, 2
+    bucket << 8
+    assert_equal bucket.mean, 4
   end
 end
